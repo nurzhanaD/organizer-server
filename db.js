4272884d0b854518526423057cd78d7e86cf8a08
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-01-18 19:09:22 +0300

    chnged to hosted db

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "postgres",
-    password: "",
-    host: "localhost",
+    password: "3YKsM9RRXbxjR0Ec02365vCJGmS6ybtt",
+    host: "dpg-cf41he1a6gdmd1idgh80-a",
     port: 5432,
     database: "organizer"
 });
