3acb3049523d7e00b4b9748ced5f5cf38fb650c6
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-01-22 23:56:11 +0300

    header policy

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const goalRouter = require("./routes/goal.routes.js");
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use('/api', userRouter);
 app.use('/api', goalRouter);
 
