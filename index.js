c29b9162d16a7ee97aed09a117537141aafcdc8a
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-01-23 00:08:06 +0300

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const PORT = process.env.PORT || 5100;
 
 const userRouter = require("./routes/user.routes.js");
@@ -7,6 +8,7 @@ const goalRouter = require("./routes/goal.routes.js");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', userRouter);
 app.use('/api', goalRouter);
