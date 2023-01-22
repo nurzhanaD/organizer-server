258c32a33d39c5f81f12374608bf4562ff46916d
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-01-23 00:00:38 +0300

    changes undo

--- index.js
+++ index.js
@@ -8,13 +8,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
 app.use('/api', userRouter);
 app.use('/api', goalRouter);
 
