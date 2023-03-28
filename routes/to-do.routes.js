4df0fa8c8b91cc1e566bd6115885d0630fa146cc
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-03-28 15:38:20 +0300

    to do s api

---
+++ routes/to-do.routes.js
@@ -0,0 +1,10 @@
+const Router = require("express");
+const router = new Router();
+const toDoController = require('../controllers/to-do.controller.js');
+
+router.post('/addToDo', toDoController.addToDo);
+router.get('/getToDos', toDoController.getAllToDos);
+router.get('/getToDos/:to_do_id', toDoController.getToDoById);
+router.delete('/deleteToDo/:to_do_id', toDoController.deleteToDo);
+
+module.exports = router;
