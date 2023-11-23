aaa8724a9b01a6a6ed364f93481bd134106be8cc
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-11-23 23:40:10 +0300

    db options order

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "nuritron",
-    password: "NY21op9jPmPthvMCOlVjB4JwV8oE3rL8",
     host: "dpg-clf4jtpil2ks738nmj90-a",
+    database: "dbname_xhz1",
+    password: "NY21op9jPmPthvMCOlVjB4JwV8oE3rL8",
     port: 5432,
-    database: "dbname_xhz1"
 });
 // const pool = new Pool({
 //     user: 'postgres',
