2c162dcca00cb55a1148db45f506506f221a6cf1
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-11-23 23:46:30 +0300

    db hostname

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "nuritron",
-    host: "dpg-clf4jtpil2ks738nmj90-a",
+    host: "postgres://nuritron:NY21op9jPmPthvMCOlVjB4JwV8oE3rL8@dpg-clf4jtpil2ks738nmj90-a.oregon-postgres.render.com/dbname_xhz1",
     database: "dbname_xhz1",
     password: "NY21op9jPmPthvMCOlVjB4JwV8oE3rL8",
     port: 5432,
