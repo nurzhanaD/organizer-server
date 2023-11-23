c6cf9644a7ddf2189ea55d8c98ff2ab676ee4bcc
nurzhana <nurzhana@MacBook-Air-admin.local>
2023-11-23 23:51:57 +0300

    new db same region as service

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
     user: "nuritron",
-    host: "postgres://nuritron:NY21op9jPmPthvMCOlVjB4JwV8oE3rL8@dpg-clf4jtpil2ks738nmj90-a.oregon-postgres.render.com/dbname_xhz1",
-    database: "dbname_xhz1",
-    password: "NY21op9jPmPthvMCOlVjB4JwV8oE3rL8",
+    host: "dpg-clfrlvef27hc739f0hfg-a",
+    database: "dbname_gcwg",
+    password: "UiNL4SWy5PtcGkpmEFGt33PvTip1wdtN",
     port: 5432,
 });
 // const pool = new Pool({
